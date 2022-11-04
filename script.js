b2897414e813a3eb8abf365bfc92513b5e62fea2
Nin Pasker <gooddev.e.nin@gmail.com>
2022-11-03 21:31:20 -0400

    SG: Added js functionality for additonal info

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ nasaApp.init = () => {
 
 }
 
+// created a method to hold the API fetch info
 nasaApp.getPicture = () => {
-
+    // Nasa API URL
     nasaApp.baseUrl = new URL(`https://api.nasa.gov/planetary/apod`);
 
         nasaApp.baseUrl.search = new URLSearchParams({
@@ -20,15 +21,17 @@ nasaApp.getPicture = () => {
             count: 1
         });
 
-    
+        
         fetch(nasaApp.baseUrl)
             .then(response => response.json())
             .then(data => {
+                // Using method created in namespace to parse the returned  object and perform a bunch of appending
                 nasaApp.showPicture(data)
                 console.log(data);
             })
 }
 
+// created a method to append the data that is passed into it
 nasaApp.showPicture = (arrayOfData) => {
 
     const pictureContainer = document.querySelector(`ul`);
@@ -37,27 +40,58 @@ nasaApp.showPicture = (arrayOfData) => {
     arrayOfData.forEach(img => {
         const pictureAchor = document.createElement(`li`);
         const pictureTitle = document.createElement(`h2`);
+        const pictureInfo = document.createElement('p');
+        
         pictureTitle.textContent = img.title;
+        pictureInfo.textContent = img.explanation;
+        
         const picture = document.createElement(`img`);
         picture.src = img.url;
         picture.alt = img.title;
+        
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.name = "curious";
+        checkbox.value = "moreInfo";
+        checkbox.id = "moreInfo";
+
+        const label = document.createElement('label');
+        label.htmlFor = "moreInfo";
+        label.textContent = "Additional Information";
 
         pictureAchor.appendChild(pictureTitle);
         pictureAchor.appendChild(picture);
+        pictureAchor.appendChild(pictureInfo);
         pictureContainer.appendChild(pictureAchor);
-        
+        pictureContainer.appendChild(checkbox);
+        pictureContainer.appendChild(label);
 
+        const checkboxListener = document.querySelector('input[type="checkbox"]')
+
+        console.log(checkboxListener);
+
+        checkboxListener.addEventListener(`input`, function(e) {
+            const checked = e.target.checked;
+
+            if (checked === true) {
+                pictureInfo.style.display = "block";
+            } else {
+                pictureInfo.style.display = "none";
+            }
+        })
     });
-    
 }
+
 nasaApp.setUpEventListeners = () => {
     const randomButton = document.querySelector(`.randomButton`);
     const imgContainer = document.querySelector('.nasaImgContainer');
+
     randomButton.addEventListener(`click`, function(e) {
         nasaApp.getPicture();
         imgContainer.style.display = "block";
         imgContainer.scrollIntoView();
     })
+
 };
 
     
